218358c304b93da77d1492cfc9d5c05668ac859b
Leemoqua <94879197+Leemoqua@users.noreply.github.com>
2024-06-29 16:04:43 +0700

    okok

---
+++ Assets/Resources/Enemy/BossModel/Boss-Level1/Script/LaserMove.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserMove : MonoBehaviour
+{
+
+    public float moveSpeed;
+     
+    protected virtual void Update(){
+        // transform.Translate(transform.rotation * moveSpeed *Time.deltaTime); 
+    }
+
+ 
+}
